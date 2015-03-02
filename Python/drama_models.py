417f45e3b81893cdda6cb1976491681c69b295dd
lauchblatt <thomas.schmidt@stud.uni-regensburg.de>
2015-03-02 17:41:53 +0100

    Current Project
    Added current project

---
+++ Python/drama_models.py
@@ -0,0 +1,215 @@
+#coding: utf8
+
+from statistic_functions import *
+
+class DramaModel:
+
+    def __init__ (self):
+        self._title = None
+        self._date = None
+        self._type = None
+        self._author = None
+        self._acts = None
+        # Zur Hilfe alle Speakers speichern, damit sie ueberhaupt irgendwo sind
+        self._speakers = None
+
+        # Kann man berechnen innerhalb der Klasse ueber Konfigurationen der Akte
+        self._configuration_matrix = None
+        self._configuration_density = None
+
+        self._replicasLength_avg = None
+        self._replicasLength_max = None
+        self._replicasLength_min = None
+        self._replicasLength_med = None
+        
+    #def calc_config_matrix (self):
+        #self._configuration_matrix = None
+
+    def calc_config_density (self):
+        sum_all = 0
+        sum_speaking = 0
+        for act in self._acts:
+            for configuration in act._configurations:
+                sum_speaking += len(configuration._appearing_speakers)
+                sum_all += len(self._speakers)
+
+        self._configuration_density = float(sum_speaking) / sum_all
+
+    def calc_speaker_relations (self):
+        for speaker in self._speakers:
+            speaker._concomitant = self.get_concomitant_speakers(speaker)
+            speaker._alternative = self.get_alternative_speakers(speaker)
+        for speaker in self._speakers:
+            self.check_dominating_status(speaker)
+        for speaker in self._speakers:
+            speaker._independent = self.get_independent_speakers(speaker)
+
+    def get_list_of_speaker_names(self, current_speaker):
+        speaker_names = []
+        for speaker in self._speakers:
+            if speaker is not current_speaker:
+                speaker_names.append(speaker._name)
+        return speaker_names
+
+    def get_concomitant_speakers (self, speaker):
+        concomitant_speakers = self.get_list_of_speaker_names(speaker)
+        for act in self._acts:
+            for configuration in act._configurations:
+                if speaker._name in configuration._appearing_speakers:
+                    concomitant_speakers = list(set(concomitant_speakers).intersection(configuration._appearing_speakers))
+        return concomitant_speakers
+
+    def get_alternative_speakers(self, speaker):
+        alternative_speakers = self.get_list_of_speaker_names(speaker)
+        for act in self._acts:
+            for configuration in act._configurations:
+                if speaker._name in configuration._appearing_speakers:
+                    alternative_speakers = list(set(alternative_speakers) - set(configuration._appearing_speakers))
+        return alternative_speakers
+
+    def check_dominating_status(self, current_speaker):
+        for speaker in self._speakers:
+            if speaker is current_speaker:
+                continue
+            if speaker._name in current_speaker._concomitant:
+                if current_speaker._name not in speaker._concomitant:
+                    current_speaker._gets_dominated_by.append(speaker._name)
+                    speaker._dominates.append(current_speaker._name)
+                    current_speaker._concomitant.remove(speaker._name)
+
+    def get_independent_speakers(self, speaker):
+        independent_speakers = self.get_list_of_speaker_names(speaker)
+        independent_speakers = list(set(independent_speakers) - set(speaker._concomitant))
+        independent_speakers = list(set(independent_speakers) - set(speaker._alternative))
+        independent_speakers = list(set(independent_speakers) - set(speaker._dominates))
+        independent_speakers = list(set(independent_speakers) - set(speaker._gets_dominated_by))
+        return independent_speakers
+
+    def get_replicas_drama(self):
+        replicas_in_drama = []
+        for act in self._acts:
+            for configuration in act._configurations:
+                for replicas in configuration._replicas:
+                    replicas_in_drama.append(replicas)
+        return replicas_in_drama
+
+    def calc_replicas_statistics(self):
+        replicas = self.get_replicas_drama()
+        replicas_lengths = []
+        for replica in replicas:
+            replicas_lengths.append(replica._length)
+
+        self._replicasLength_avg = average(replicas_lengths)
+        self._replicasLength_max = max(replicas_lengths)
+        self._replicasLength_min = min(replicas_lengths)
+        self._replicasLength_med = median(replicas_lengths)
+
+    def add_replicas_to_speakers(self):
+        replicas = self.get_replicas_drama()
+        for replica in replicas:
+            for speaker in self._speakers:
+                if(replica._speaker == speaker._name):
+                    speaker._replicas.append(replica)
+                    break
+
+class ActModel:
+
+    def __init__ (self):
+        self._number = None
+        #Akt besteht aus Konfigurationen
+        self._configurations = None
+
+        self._replicasLength_avg = None
+        self._replicasLength_max = None
+        self._replicasLength_min = None
+        self._replicasLength_med = None        
+
+    def get_replicas_act(self):
+        replicas_in_act = []
+        for configuration in self._configurations:
+            for replicas in configuration._replicas:
+                replicas_in_act.append(replicas)
+        return replicas_in_act
+
+    def calc_replicas_statistics(self):
+        replicas = self.get_replicas_act()
+        replicas_lengths = []
+        for replica in replicas:
+            replicas_lengths.append(replica._length)
+
+        self._replicasLength_avg = average(replicas_lengths)
+        self._replicasLength_max = max(replicas_lengths)
+        self._replicasLength_min = min(replicas_lengths)
+        self._replicasLength_med = median(replicas_lengths)
+
+class ConfigurationModel:
+    def __init__ (self):
+        #Waere vielleicht nicht schlecht den Namen, also 1.Akt, 1.Szene zu speichern
+        self._name = None
+        self._number = None
+        #Konfiguration besteht aus Repliken
+        self._replicas = None
+        # um spaeter leichter Beziehungen zu berechnen, als namen
+        self._appearing_speakers = None
+
+        self._replicasLength_avg = None
+        self._replicasLength_max = None
+        self._replicasLength_min = None
+        self._replicasLength_med = None
+
+    def calc_replicas_statistics(self):
+        replicas = self._replicas
+        replicas_lengths = []
+        for replica in replicas:
+            replicas_lengths.append(replica._length)
+
+        print(replicas_lengths)
+        self._replicasLength_avg = average(replicas_lengths)
+        self._replicasLength_max = max(replicas_lengths)
+        self._replicasLength_min = min(replicas_lengths)
+        self._replicasLength_med = median(replicas_lengths)
+
+class ReplicaModel:
+
+    def __init__ (self):
+        self._id = None
+        self._length = None
+        #Eine Replik hat einen zugewiesenen Speaker, Name des Speakers speichern
+        self._speaker = None
+
+
+class SpeakerModel:
+
+    def __init__ (self):
+        self._name = None   
+        self._alternative_names = None
+        self._replicas = []
+        self._concomitant = []
+        self._alternative = []
+        self._dominates = []
+        self._gets_dominated_by = []
+        self._independent = []
+        # self._repetitive_config = None
+
+        self._replicasLength_avg = None
+        self._replicasLength_med = None
+        self._replicasLength_min = None
+        self._replicasLength_max = None
+
+    def calc_replicas_statistics(self):
+        replicas_lengths = []
+        for replicas in self._replicas:
+            replicas_lengths.append(replicas._length)
+
+        self._replicasLength_avg = average(replicas_lengths)
+        self._replicasLength_max = max(replicas_lengths)
+        self._replicasLength_min = min(replicas_lengths)
+        self._replicasLength_med = median(replicas_lengths)
+        print(self._name)
+        print(self._replicasLength_avg)
+        print(self._replicasLength_max)
+        print(self._replicasLength_min)
+        print(self._replicasLength_med)
+
+
+
---
+++ Python/drama_parser.py
@@ -0,0 +1,235 @@
+#coding: utf8
+
+import xml.etree.ElementTree as ET
+import re
+from drama_models import *
+
+class DramaParser:
+
+    namespaces = {'tei':'http://www.tei-c.org/ns/1.0'} # used to read the tags in the xml correctly
+
+    def parse_xml(self, filepath):
+        drama_model = DramaModel()
+        xml_root = self.get_xml_root(filepath) # get xml treeroot
+
+        # create speaker mapping
+        self.speaker_mapping(xml_root)
+
+        # general information
+        drama_model._title = self.get_title(xml_root)
+        drama_model._author = self.get_author(xml_root)
+        drama_model._date = self.get_date(xml_root)
+        drama_model._type = self.get_type(filepath)
+
+        drama_model._subact_type = self.get_subact_type(xml_root)
+        drama_model._acts = self.extract_act_data(xml_root)
+        drama_model._speakers = self.get_all_speakers(xml_root)
+        self.get_speakers_from_castgroup(xml_root)
+
+        drama_model.calc_config_density()
+        drama_model.calc_speaker_relations()
+        drama_model.calc_replicas_statistics()
+        print (vars(drama_model))
+
+        for act in drama_model._acts:
+            act.calc_replicas_statistics()
+            for configuration in act._configurations:
+                print(configuration._name)
+                configuration.calc_replicas_statistics()
+
+        drama_model.add_replicas_to_speakers()
+        for speaker in drama_model._speakers:
+            speaker.calc_replicas_statistics()
+            print (vars(speaker))
+
+
+
+    def get_xml_root(self, filepath):
+        tree = ET.parse(filepath)
+        return tree.getroot()
+
+    def get_title(self, xml_root):
+        title = xml_root.find(".//tei:fileDesc/tei:titleStmt/tei:title", self.namespaces).text
+        return title
+
+    def get_author(self, xml_root):
+        author = xml_root.find(".//tei:sourceDesc/tei:biblFull/tei:titleStmt/tei:author", self.namespaces).text
+        return author
+
+    def get_date(self, xml_root):
+        date = xml_root.find(".//tei:profileDesc/tei:creation/tei:date", self.namespaces).attrib
+        return date
+
+    def get_type(self, filepath):
+        if filepath.find("_s.xml") != -1:
+            return "Schauspiel"
+        elif filepath.find("_t.xml") != -1:
+            return "Trauerspiel"
+        elif filepath.find("_k.xml") != -1:
+            return "Komoedie"
+
+    def get_subact_type(self, xml_root):
+        subact_type = xml_root.find(".//tei:div[@type='act']/tei:div[@subtype='work:no']//tei:desc/tei:title", self.namespaces).text
+        if subact_type.find("Auftritt") != -1:
+            return "Auftritt"
+        elif subact_type.find("Szene") != -1:
+            return "Szene"
+
+    # every speaker, even if they are double with different names
+    def get_all_speakers(self, xml_root, as_objects = True):
+        speaker_list = []
+        for speaker in xml_root.findall(".//tei:speaker", self.namespaces):
+
+            name = speaker.text
+            if name[-1] == ".":
+                name = name[:-1]
+            if name not in speaker_list:
+                speaker_list.append(name)
+
+        if not as_objects:
+            return speaker_list
+
+        speaker_model_list = []
+
+        for speaker in speaker_list:
+            speaker_model = SpeakerModel()
+            speaker_model._name = speaker
+            speaker_model_list.append(speaker_model)
+        return speaker_model_list
+
+    # persons which are listed in the beginning
+    def get_speakers_from_castgroup(self, xml_root):
+        castgroup = []
+        for actor in xml_root.findall(".//tei:castGroup/tei:castItem", self.namespaces):
+            real_name = actor.text
+
+            if "," in real_name:
+                comma = real_name.index(",")
+                real_name = real_name[:comma]
+            elif real_name[-1] == ".":
+                real_name = real_name[:-1]
+
+            castgroup.append(real_name)
+
+        return castgroup
+
+    def extract_act_data(self, xml_root):
+        act_data = []
+        position = 1
+        # gets number of acts and corresponding count of scenes
+        for act in xml_root.findall(".//tei:div[@type='act']", self.namespaces):
+
+            # number_of_scenes = len(act.findall("./tei:div[@subtype='work:no']", self.namespaces))
+            act_model = ActModel()
+            act_model._number = position
+            act_model._configurations = self.extract_subact_data(act, position)
+            act_data.append(act_model)
+            position += 1
+
+        return act_data
+
+    def extract_subact_data(self, act, position):
+        config_data = []
+        subact_position = 1
+        for subact in act.findall("./tei:div[@subtype='work:no']", self.namespaces):
+            config_model = ConfigurationModel()
+            config_model._number = subact_position
+            config_model._name = str(position) + " - " + str(subact_position)
+            config_model._replicas = self.get_replicas_for_subact(subact)
+            config_model._appearing_speakers = self.get_speakers_for_subact(subact)
+            config_data.append(config_model)
+            subact_position += 1
+        return config_data
+
+    def get_replicas_for_subact(self, subact):
+        replica_data = []
+
+        for subact_speaker_wrapper in subact.findall(".//tei:sp", self.namespaces):
+            replica_model = ReplicaModel()
+            subact_speaker = subact_speaker_wrapper.find("./tei:speaker", self.namespaces)
+            name = subact_speaker.text
+            if name[-1] == ".":
+                name = name[:-1]
+
+            replica_model._speaker = name
+            replica_model._length = self.get_replica_length(subact_speaker_wrapper)
+            replica_data.append(replica_model)
+
+        return replica_data
+
+    def get_replica_length(self, sub_sp_wrapper):
+        length = 0
+        stage_dir_length = 0
+        p_tag = sub_sp_wrapper.find("./tei:p", self.namespaces)
+        l_tag = sub_sp_wrapper.find("./tei:l", self.namespaces)
+
+        if p_tag is not None:
+            for element in p_tag.findall("./tei:hi[@rend='italic']", self.namespaces):
+                stage_dir_length += self.get_wordcount_from_string(element.text)
+            for text in p_tag.itertext():
+                length += self.get_wordcount_from_string(text)
+            length = length - stage_dir_length
+
+        elif l_tag is not None:
+            for element in l_tag.findall("./tei:hi[@rend='italic']", self.namespaces):
+                stage_dir_length += self.get_wordcount_from_string(element.text)
+            for text in l_tag.itertext():
+                length += self.get_wordcount_from_string(text)
+            length = length - stage_dir_length
+
+        #Für klassische Dramen, deren Zeilenwechsel notiert wurde
+        if l_tag is None:
+            lg_tag = sub_sp_wrapper.findall("./tei:lg", self.namespaces)
+            for lg_element in sub_sp_wrapper.findall("./tei:lg", self.namespaces):
+                for l_element in lg_element.findall("./tei:l", self.namespaces):
+                    length += self.get_wordcount_from_string(l_element.text)
+
+        return length
+
+    def get_wordcount_from_string(self, text):
+        word_list = re.sub("[^\w]", " ", text).split()
+        return len(word_list)
+
+
+    def get_speakers_for_subact(self, subact):
+        speaker_data = []
+
+        for subact_speaker in subact.findall(".//tei:speaker", self.namespaces):
+            name = subact_speaker.text
+            if name[-1] == ".":
+                name = name[:-1]
+            if name not in speaker_data:
+                speaker_data.append(name)
+
+        return speaker_data
+
+    def speaker_mapping(self, xml_root):
+        castgroup = self.get_speakers_from_castgroup(xml_root)
+        all_speakers = self.get_all_speakers(xml_root, False)
+        print (castgroup)
+        print (all_speakers)
+
+        # stopwortliste!!!!!!
+        # compare lists
+
+        #for castgroup_member in castgroup:
+            #for speaker in all_speakers:
+
+
+
+def main():
+    parser = DramaParser()
+    parser.parse_xml('Korpus/less_nathan_s.xml')
+
+
+if __name__ == "__main__":
+    main()
+
+
+    # Ideen fürs Mapping:
+    # wenn nur eine Person pro Tag drinsteht, ein Name vor dem Komma, mehrere nach dem Komma -> Stoppwortliste
+    # 'gemm_hausvater' überprüfen, ob mehrere Punkte genutzt werden und einfach alles ignorieren?
+    # 'zwsch_abbelino': mehreren Personen eine Bezeichnung zugewiesen, zB Diener, 
+    #       aber das steht erst einige Zeilen weiter unten: erkennbar: Die Zeilen schließen nicht mit Punkt ab???
+
+    # gemm_hausvater hat noch handlungen????!!!
