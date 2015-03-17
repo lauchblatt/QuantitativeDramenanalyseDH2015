a9c4df4d219bde8b6b80a502e94788a2312793d7
lauchblatt <thomas.schmidt@stud.uni-regensburg.de>
2015-03-17 11:25:28 +0100

    Displaying all Dramas

--- FrontEnd/js/DramaListView.js
+++ FrontEnd/js/DramaListView.js
@@ -6,7 +6,21 @@ DramaAnalyzer.DramaListView = function(){
 	};
 
 	var renderList = function(dramaList){
-		console.log(dramaList);
+		for(var i = 0; i < dramaList.length; i++){
+			console.log(dramaList[i]);
+			var row = createListItem(dramaList[i]);
+			$("#table-tbody").append(row);
+		}
+	};
+
+	var createListItem = function(drama){
+		var row = $("<tr>");
+		row.append(($("<td>")).text("0"));
+		row.append(($("<td>")).text(drama.Title));
+		row.append(($("<td>")).text(drama.Author));
+		row.append(($("<td>")).text(drama.Type));
+		row.append(($("<td>")).text(drama.Date));
+		return row;
 	};
 
 	that.init = init;
