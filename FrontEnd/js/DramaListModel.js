d5661e17ab18e3f0bbcbcb7eb7ae01bb7a777931
Thomas Schmidt <thomas.schmidt@stud.uni-regensburg.de>
2015-03-22 16:07:01 +0100

    Added Retrieval for number of speeches

--- FrontEnd/js/DramaListModel.js
+++ FrontEnd/js/DramaListModel.js
@@ -24,7 +24,12 @@ Search.DramaListModel = function(){
 		}
 
 		if('year' in input){
-			retrieveDataByRange(input['year'].from, input['year'].to, 'year')
+			retrieveDataByRange(input['year'].from, input['year'].to, 'year');
+		}
+		
+		if('number_of_speeches_in_drama' in input){
+			retrieveDataByRange(input['number_of_speeches_in_drama'].from, 
+				input['number_of_speeches_in_drama'].to, 'number_of_speeches_in_drama');
 		}
 	};
 
@@ -33,7 +38,7 @@ Search.DramaListModel = function(){
 		firebaseRef = null;
 		firebaseRef = new Firebase("https://popping-heat-510.firebaseio.com/drama_data");
 		if(from === undefined){from = 0};
-		if(to === undefined){to = new Date().getFullYear()};
+		if(to === undefined){to = ""};
 		firebaseRef.orderByChild(attribute).startAt(from).endAt(to).on("child_added", function(snapshot) {
 			$(that).trigger("DataRetrieved", [snapshot.val()]);
 		}, function (errorObject) {
--- FrontEnd/js/FormsView.js
+++ FrontEnd/js/FormsView.js
@@ -13,14 +13,21 @@ Search.FormsView = function(){
 		var author = $("#input-author").val();
 		var date_from = $("#input-date-from").val();
 		var date_to = $("#input-date-to").val();
+		var numberOfSpeeches_from = $("#input-numberOfSpeeches-from").val();
+		var numberOfSpeeches_to = $("#input-numberOfSpeeches-to").val();
 
 		if(title != ""){input["title"] = title;}
 		if(author != ""){input["author"] = author;}
 
-		var range = {}
+		var range = {};
 		if(date_from != ""){range.from = parseInt(date_from);}
 		if(date_to != ""){range.to = parseInt(date_to);}
 		if(date_from != "" || date_to != ""){input["year"] = range;}
+
+		var range = {};
+		if(numberOfSpeeches_from != ""){range.from = parseInt(numberOfSpeeches_from);}
+		if(numberOfSpeeches_to  != ""){range.to = parseInt(numberOfSpeeches_to);}
+		if(numberOfSpeeches_from != "" || numberOfSpeeches_to  != ""){input["number_of_speeches_in_drama"] = range;}
 
 		$(that).trigger("InputCatched", [input]);
 	};
