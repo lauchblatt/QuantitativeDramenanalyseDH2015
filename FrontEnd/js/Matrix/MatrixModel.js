134ff44ab72966a8fefec3f6e91a5dfd870df441
Thomas Schmidt <thomas.schmidt@stud.uni-regensburg.de>
2015-03-24 05:01:05 +0100

    Algorithm to create matrix
    ...consisting of 0 and 1

--- FrontEnd/js/Matrix/MatrixModel.js
+++ FrontEnd/js/Matrix/MatrixModel.js
@@ -3,11 +3,18 @@ Matrix.MatrixModel = function(){
 	var currentDrama_id = 1;
 	//!!! Important for future dramas with for example only one act
 	//Only works, if dramaInfo is an Object, scenesInfo, actsInfo, speakersInfo is Array
+
+	/* Globals to work with db-Info */
 	var dramaInfo = null;
 	var scenesInfo = [];
 	var actsInfo = [];
 	var speakersInfo = [];
 	var firebaseRef = null; 
+
+	/* Fields to calculate and represent matrix */
+	var speakersNames = [];
+	/* Two Dimensional Arrays are not supported for javascript */
+	var matrix = [];
 
 	var init = function(){
 		$(that).on("InitFinished", continueInit);
@@ -18,12 +25,55 @@ Matrix.MatrixModel = function(){
 	};
 
 	var continueInit = function(){
-		console.log(dramaInfo);
-		console.log(scenesInfo);
-		console.log(actsInfo);
-		console.log(speakersInfo);
 		if(dramaInfo && scenesInfo.length > 0 && actsInfo.length > 0 && speakersInfo.length > 0){
 			console.log("everything is set");
+			speakersNames = dramaInfo.speakers;
+			calculateMatrix();
+		}
+	};
+
+	var calculateMatrix = function(){
+		/* Init Two Dimensional Matrix */
+		initMatrix();
+
+		for(var i = 0; i < dramaInfo.speakers.length; i++){
+			sceneCounter = 0;
+			for(var j = 0; j < scenesInfo.length; j++){
+				for(var k = 0; k < scenesInfo[j].length; k++){
+					console.log('###');
+					console.log("Akt " + j);
+					console.log("Scene " + k);
+					console.log(dramaInfo.speakers[i]);
+					console.log(scenesInfo[j][k].appearing_speakers);
+					
+					var speakerAppears = checkIfSpeakerInList(dramaInfo.speakers[i],
+						scenesInfo[j][k].appearing_speakers);
+					if(speakerAppears){
+						matrix[i][sceneCounter] = 1;
+					}else{
+						matrix[i][sceneCounter] = 0;
+					}
+					sceneCounter++;
+					console.log(speakerAppears);
+					console.log('###');
+				}
+			}
+		}
+		console.log(matrix);
+	};
+
+	var checkIfSpeakerInList = function(speaker, speakerList){
+		if (speakerList.indexOf(speaker) > -1) {
+    		return true;
+		} else {
+    		return false;
+		}
+	};
+
+	var initMatrix = function(){
+		matrix = new Array(dramaInfo.speakers.length);
+		for(var i = 0; i < dramaInfo.speakers.length; i++){
+			matrix[i] = new Array(dramaInfo.number_of_scenes);
 		}
 	};
 
