331f3cad9a60f9870dac0605a7984bb5e7fd5e2d
Thomas Schmidt <thomas.schmidt@stud.uni-regensburg.de>
2017-06-16 11:07:41 +0200

    Creating BackCopy, startin sentiment Analysis

---
+++ Python/sa_models.py
@@ -0,0 +1,41 @@
+#coding: utf8
+
+import os
+import re
+import collections
+import locale
+import sys
+
+def main():
+	reload(sys)
+	sys.setdefaultencoding('utf8')
+
+class sentimentInformation:
+	#List of SentimentBearingWord-Objects
+	self._sentimentBearingWords = []
+
+class sentimentBearingWord:
+
+	#SentiWSData
+	self._positiveSentiWS = 0
+	self._negativeSentiWS = 0
+
+	#NRCData
+	self._positiveNrc = 0
+	self._negativeNrc = 0
+	self._anger = 0
+	self._anticipation = 0
+	self._disgust = 0
+	self._fear = 0
+	self._joy = 0
+	self._sadness = 0
+	self._surprise = 0
+	self._trust = 0
+
+	#Language Data
+	self._token = ""
+	self._lemma = ""
+	self._POS = ""
+
+if __name__ == "__main__":
+    main()
---
+++ Python/sentiment_analysis.py
@@ -0,0 +1,19 @@
+#coding: utf8
+
+import os
+import re
+import collections
+import locale
+import sys
+
+def main():
+	reload(sys)
+	sys.setdefaultencoding('utf8')
+
+class Sentiment_Analyzer:
+
+	def attachSAInformationToSingleDrama(self, dramaModel):
+		
+
+if __name__ == "__main__":
+    main()
