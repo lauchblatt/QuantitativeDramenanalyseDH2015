45f060d2fe302751c1c7d48365ee1f5cee658b91
Thomas Schmidt <thomas.schmidt@stud.uni-regensburg.de>
2017-09-01 16:54:58 +0200

    actsScenesStuff ready

--- FrontEnd/js/SA_ActsScenes/ActsScenesView.js
+++ FrontEnd/js/SA_ActsScenes/ActsScenesView.js
@@ -2,13 +2,15 @@ ActsScenes.ActsScenesView = function(){
 	var that = {};
 	var metricsForActs = [];
 	var actsProportionData = {};
+	var metricsForScenes = [];
 
-	var init = function(metricsActs, proportionDataForActs){
+	var init = function(metricsActs, proportionDataForActs, metricsScenes){
 		metricsForActs = metricsActs;
 		actsProportionData = proportionDataForActs;
+		metricsForScenes = metricsScenes;
 
-		initNumberOfActs(actsProportionData.length)
-		initListener()
+		initNumberOfActs(actsProportionData.length);
+		initListener();
 
 	};
 
@@ -29,6 +31,9 @@ ActsScenes.ActsScenesView = function(){
 		$("#selection-acts-pie-number").change(renderActPieChart);
 		$("#selection-acts-pie-metric").change(renderActPieChart);
 		$("#selection-acts-pie-type").change(renderActPieChart);
+
+		$("#selection-scenesPerAct-bar-metric").change(renderScenesPerActBars);
+		$("#selection-scenesPerAct-bar-normalisation").change(renderScenesPerActBars);
 	};
 
 	var renderActPieChart = function(){
@@ -59,29 +64,40 @@ ActsScenes.ActsScenesView = function(){
 	};
 
 	var renderActsBars = function(){
-		metricSelection = $("#selection-act-bar-metric").val();
-		normalisationSelection = $("#selection-act-bar-normalisation").val()
-		metric = transformGermanMetric(metricSelection);
-		normalisation = transformGermanMetric(normalisationSelection);
-		metrics = getActsMetricsByName(normalisation, metric);
+		var metricSelection = $("#selection-act-bar-metric").val();
+		var normalisationSelection = $("#selection-act-bar-normalisation").val()
+		var metric = transformGermanMetric(metricSelection);
+		var normalisation = transformGermanMetric(normalisationSelection);
+		var metrics = getActsMetricsByName(normalisation, metric);
+
 		drawBarChartAct(normalisationSelection, metricSelection, metrics);
 	};
 
-	//Loop to render all Graphs for Scenes dynamically
-	var drawChartScenes = function(scenesInfo){
-		$charts_scenes = $("#charts-scenes");
-		for(var act = 0; act < scenesInfo.length; act++){
-			$div_chart = $("<div></div>");
-			$div_chart.addClass("scenes-chart");
-			$div_chart.attr("id", "chart-div-scenes-" + act);
-			$charts_scenes.append($div_chart);
-			var $button = $("<button class='btn btn-primary png-download'></button>");
-			$button.attr("id", "download-png-" + act);
-			$button.text("Download PNG");
-			var $buttonDiv = $("<div>").addClass("container").append($button);
-			$charts_scenes.append($buttonDiv);
-			drawChartForScenesInAct(("chart-div-scenes-" + act), scenesInfo[act], (act+1));
+	var renderScenesPerActBars = function(){
+		var metricSelection = $("#selection-scenesPerAct-bar-metric").val();
+		var normalisationSelection = $("#selection-scenesPerAct-bar-normalisation").val()
+		var metric = transformGermanMetric(metricSelection);
+		var normalisation = transformGermanMetric(normalisationSelection);
+		var metrics = getScenesPerActMetrics(normalisation, metric);
+		drawChartScenesPerAct(normalisationSelection, metricSelection, metrics);
+		
+	}
+
+	var getScenesPerActMetrics = function(normalisation, metricName){
+		var specificMetricsForScenes = []
+		for(var i = 0; i < metricsForScenes.length; i++){
+			var specificMetricsForScenesPerAct = [];
+			for(var j = 0; j < metricsForScenes[i].length; j++){
+				var metric = metricsForScenes[i][j][normalisation][metricName];
+				var sceneNumber = j+1
+				var annotation = (Math.round(metric*10000)/10000).toString();
+				var tuple = [sceneNumber, metric, annotation];
+				specificMetricsForScenesPerAct.push(tuple);
+			}
+			specificMetricsForScenes.push(specificMetricsForScenesPerAct);
 		}
+		console.log(specificMetricsForScenes);
+		return specificMetricsForScenes;
 	};
 
 	var getActsMetricsByName = function(normalisation, metricName){
@@ -89,10 +105,77 @@ ActsScenes.ActsScenesView = function(){
 		for(var i = 0; i < metricsForActs.length; i++){
 			metric = metricsForActs[i][normalisation][metricName];
 			actName = (i+1).toString() + " .Akt";
-			tuple = [actName, metric, (Math.round(metric * 100) / 100).toString()];
+			tuple = [actName, metric, (Math.round(metric * 10000) / 10000).toString()];
 			specificMetricsForActs.push(tuple);
 		}
 		return specificMetricsForActs;
+	};
+
+	//Loop to render all Graphs for Scenes dynamically
+	var drawChartScenesPerAct = function(normalisation, metricName, scenesMetricsPerAct){
+
+		$charts_scenes = $("#charts-scenes");
+		for(var act = 0; act < scenesMetricsPerAct.length; act++){
+			$div_chart = $("<div></div>");
+			$div_chart.addClass("scenes-chart");
+			$div_chart.attr("id", "chart-div-scenes-" + act);
+			$charts_scenes.append($div_chart);
+			var actNumber = act + 1;
+			drawBarChartForScenesInAct(("chart-div-scenes-" + act), normalisation, 
+				metricName, scenesMetricsPerAct[act], actNumber);
+		}
+	};
+
+	var drawBarChartForScenesInAct = function(divId, normalisation, metricName, metrics, actNumber){
+        var vAxisTitle = metricName + " - " + normalisation
+		var data = new google.visualization.DataTable();
+		data.addColumn("number", "Scene-Number")
+		data.addColumn("number", metricName)
+		data.addColumn({type:'string', role:'annotation'})
+
+        data.addRows(metrics);
+        //necessary to have consistent gaps according to the scenes on the graph 
+        var ticksArray = [];
+        for(var k = 0; k < metrics.length; k++){
+        	ticksArray.push(k+1);
+        }
+        var options = {title:'Szenen-Verlauf: ' + actNumber + " Akt",
+        			   height: 600,
+        			   width: 1170,
+        			   chartArea:{width:'70%',height:'75%'},
+        			    trendlines: {
+				          0: {
+				          	tooltip: false,
+				            type: 'polynomial',
+				            color: 'green',
+				            lineWidth: 3,
+				            opacity: 0.3,
+				            showR2: false,
+				            visibleInLegend: false
+				          }
+				        },
+				        hAxis: {
+        			   	title: 'Szenen',
+        			   	ticks: ticksArray
+        			   },
+        			   vAxis: {
+        			   	title: vAxisTitle,
+        			   	baseline: 0
+        			   },
+                   	   animation: {
+                   	   	duration: 700,
+                   	   	startup: true
+                   	   }};
+        
+        var formatter = new google.visualization.NumberFormat(
+    		{fractionDigits: 6});
+		formatter.format(data, 1); // Apply formatter to second column
+
+        var chart = new google.visualization.ColumnChart(document.getElementById(divId));
+
+        var chart_div = document.getElementById(divId);
+
+        chart.draw(data, options);
 	};
 
 	var drawBarChartAct = function(normalisation, metricName, numberMetricPairs){
@@ -102,7 +185,7 @@ ActsScenes.ActsScenesView = function(){
 		data.addColumn("number", metricName)
 		data.addColumn({type:'string', role:'annotation'})
         data.addRows(numberMetricPairs);
-        var options = {title:'Akt-Statistik',
+        var options = {title:'Akt-Verlauf: ' + vAxisTitle,
         			   height: 600,
         			   width: 1130,
         			   chartArea:{width:'70%',height:'75%'},
@@ -122,13 +205,16 @@ ActsScenes.ActsScenesView = function(){
         			   },
         			   vAxis: {
         			   	title: vAxisTitle,
-        			   	baseline: 0
+        			   	baseline: 0,
         			   },
                    	   animation: {
                    	   	duration: 700,
                    	   	startup: true
                    	   }};
 
+        var formatter = new google.visualization.NumberFormat(
+    		{fractionDigits: 6});
+		formatter.format(data, 1); // Apply formatter to second column
         
         var chart = new google.visualization.ColumnChart(document.getElementById('chart-div-act'));
         var chart_div = document.getElementById('chart-div-act');
@@ -203,6 +289,7 @@ ActsScenes.ActsScenesView = function(){
 		    	return "normalisedAllWords";
 		    	break;
 		    default:
+		    	console.log(name);
 		        console.log("ERROR")
 		    }
 	};
@@ -210,6 +297,7 @@ ActsScenes.ActsScenesView = function(){
 	that.init = init;
 	that.renderActsBars = renderActsBars;
 	that.renderActPieChart = renderActPieChart;
+	that.renderScenesPerActBars = renderScenesPerActBars;
 
 	return that;
 };
