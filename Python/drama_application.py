377cff616d4508756930ac3230856b306754cd94
Thomas Schmidt <thomas.schmidt@sprachlit.uni-regensburg.de>
2018-10-31 09:53:01 +0100

    some work for literary scholar

--- Python/drama_application.py
+++ Python/drama_application.py
@@ -14,13 +14,39 @@ def main():
 
     debug = True
 
+    for filename in os.listdir("Korpus"):
+        parser = DramaParser()
+        dramaModel = parser.parse_xml("Korpus/" + filename)
+        #print filename
+        numberOfAllSpeaker = float(len(dramaModel._speakers))
+        acts = dramaModel._acts
+        lastAct = acts[len(dramaModel._acts)-1]
+        confs = lastAct._configurations
+        lastConf = confs[len(lastAct._configurations)-1]
+        #print len(lastConf._appearing_speakers)
+        numberOfFinalSpeakers = float(len(lastConf._appearing_speakers))
+        proportion = numberOfFinalSpeakers/numberOfAllSpeaker
+        line = [dramaModel._title, dramaModel._author, dramaModel._type]
+        line.append(str(len(dramaModel._speakers)))
+        line.append(str(int(numberOfFinalSpeakers)))
+        line.append(str(proportion).replace(".", ","))
+        printLine = "\t".join(line)
+        print printLine
+        #print proportion
+        #if (proportion == 1.0):
+            #print dramaModel._title + " von " + dramaModel._author
+            #print "true"
+        #else:
+           # print "false"
     # used to generate one JSON file
     
+    """
     parser = DramaParser()
     dramaModel = parser.parse_xml("../Lessing-Dramen/-Der_junge_Gelehrte.xml")
     output = DramaOutput()
     drama_data = output.generate_drama_data(dramaModel)
     output.write_JSON(drama_data)
+    """
 
     """
     # used to generate a JSON file of all dramas
